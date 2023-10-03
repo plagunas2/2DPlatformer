b022013a7361fe373710dc099292faf475ada093
plagunas2 <priscilagunas@gmail.com>
2023-10-03 16:00:59 +0200

    door animation
    door opens once keycard is acquired

---
+++ MidtermPlatformer/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    public Sprite[] sprites;
+
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        sprite = this.gameObject.GetComponent<SpriteRenderer>();
+        
+        this.gameObject.GetComponent<Collider2D>().enabled = true; //collider enabled at start of level (door closed)
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sprite.sprite == sprites[sprites.Length - 1])
+        {
+            this.gameObject.GetComponent<Collider2D>().enabled = false; //collider disabled when door open -> proceed to next level
+        } 
+    }
+
+    public IEnumerator OpenDoor()
+    {
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            sprite.sprite = sprites[i];
+            yield return new WaitForSeconds(.25f);
+        }
+    }
+}
+
---
+++ MidtermPlatformer/Assets/Scripts/KeyCardUse.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCardUse : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Boolean hasKeyCard = false;
+    public GameObject door;
+    public Boolean test = false;
+
+    private DoorManager doorManager;
+    void Start()
+    {
+        doorManager = door.GetComponent<DoorManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.gameObject.tag == "KeyCard") //pick up Key Card
+        {
+            hasKeyCard = true;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "KeyCardScreen" && hasKeyCard == true && Input.GetKey(KeyCode.E)) //open door to next level
+        {
+            StartCoroutine(doorManager.OpenDoor());
+
+        }
+    }
+}
